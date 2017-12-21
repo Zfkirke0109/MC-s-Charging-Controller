53d4e83a75f5f87a49d74e33a83a15b9e9177aed
sjayminsgithub <sjaymin.us@gmail.com>
2017-12-21 17:41:10 +0530

    Initial sync

---
+++ README.md
@@ -0,0 +1,4 @@
+# MC's Charging Controller (mcc)
+# mcc README
+# versionCode=2017122003
+# MCMotheEffin' @ XDA Developers
---
+++ common/service.sh
@@ -0,0 +1,37 @@
+#!/system/bin/sh
+# mcc service
+# versionCode=2017122003
+# MCMotherEffin' @ XDA Developers
+
+# Define variables & functions
+
+mod_dir=${0%/*}
+
+data_dir=$mod_dir/data
+config=$data_dir/settings.conf
+dinfo=$data_dir/device_info.inf
+
+get_prop() { sed -n s/^$1=//p $config; }
+set_prop() { sed -i s/^$1=.*/$1=$2/g $config; }
+
+switch=$(get_prop switch | awk '{print $1}')
+
+# Update device info (debugging only)
+
+getprop | grep product >/$dinfo
+
+# Re-configure kernel references unless done
+
+if [ $switch != not_found_yet ]; then
+
+  # Set kernel switch writable
+
+  chown 0:0 $switch
+  chmod 644 $switch
+
+fi
+
+# Run in MAGISK daemon mode
+
+export no_logging=true
+(cs --daemon_trigger) &
